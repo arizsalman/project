9347a3823410b3760e041f7218aa48782ce74452
arizsalman <muhammadarizsalman@gmail.com>
2024-05-10 14:22:57 +0500

    ok

---
+++ pro.js
@@ -0,0 +1,48 @@
+// var expense_html = document.getElementById('expense')
+// var calculate_html = document.getElementById('calcualteExpense')
+// var salary_html = document.getElementById('salary')
+
+// var total_income = document.getElementById('total_income')
+// var total_expense = document.getElementById('total_expense')
+// var total_saving = document.getElementById('total_saving')
+
+// var expense = 0
+// var salary = 0
+// var savings = 0;
+
+// function calcualteExpense() {
+//     expense = expense_html.value + expense;
+//     savings = salary - expense
+
+//     total_expense.innerText = expense
+//     total_saving.innerText = savings
+// }
+// function salary() {
+//     expense = expense_html.value + salary;
+//     savings = salary - expense
+//     expense.innertext = expense;
+
+// }
+
+
+var amoumt_js=document.getElementById("amount");
+var income_js=document.getElementById("income");
+var expense_js=document.getElementById("expense");
+var totalIncome =150000;
+var expense=0;
+var saving=0;
+var tExpense_js=document.getElementById("total_expense");
+var tSaving_js=document.getElementById("total_saving");
+
+function expense() {
+    expense=expense_js.value+expense
+    totalIncome=saving-expense;
+    tExpense_js.innerText=expense;
+    tSaving_js.innerText=saving;
+    
+}
+function saving(){
+    expense=expense_js.value+expense
+    totalIncome=saving-expense;
+    expense.innerText=expense;
+}
